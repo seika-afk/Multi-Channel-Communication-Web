21879ececcf93c988b7eef1d6e2b9dea4625c8bf
Seika <artistnoob6@gmail.com>
2026-03-27 02:13:59 +0530

    added shadcn ui

---
+++ app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+	  <div className="h-screen w-screen flex gap-0.5 items-center justify-center">
+ <h1>Hello world</h1>
+
+ <Button variant={"outline"}> OK! </Button>
+</div>
+  );
+}
