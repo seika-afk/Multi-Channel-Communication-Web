5801d4099d9bc2968575285aa0d793c2756ba854
Seika <artistnoob6@gmail.com>
2026-03-27 03:15:23 +0530

    added theme change

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,18 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-	  <div className="h-screen w-screen flex gap-0.5 items-center justify-center">
- <h1>Hello world</h1>
+	  <div className="h-screen w-screen flex gap-2 items-center justify-center flex-col">
 
- <Button variant={"outline"}> OK! </Button>
+	  <UserButton
+		afterSignOutUrl="/"
+	  />
+<ModeToggle/>
+
+	  <h1>Hello world</h1>
+
 </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,11 +25,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+	  <ClerkProvider>
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={cn("min-h-full flex flex-col dark:bg-[#313338]")}>
+
+<ThemeProvider
+	 attribute="class"
+            defaultTheme="dark"
+            enableSystem={false} 
+	storageKey="pairleet-theme"
+>
+      {children}
+</ThemeProvider>
+ 
+      </body>
     </html>
+         </ClerkProvider>
   );
 }
