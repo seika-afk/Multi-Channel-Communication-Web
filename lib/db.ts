81113c121f8b73dcd09a48acd05109c9399eab09
Seika <artistnoob6@gmail.com>
2026-03-29 02:42:35 +0530

    added: profiling:prisma Store User profile

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,16 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-declare global{
-	var prisma : PrismaClient | undefined
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
 }
-
-
-export const db = globalThis.prisma || new PrismaClient();
-
-
-if (process.env.NODE_ENV!=="production") globalThis.prisma= db
